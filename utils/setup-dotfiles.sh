d8b288e5cdaf6dd4f832fb0ed8f62eb528ee42d9
Jason Lieb <Jason.lieb@outlook.com>
2023-12-03 00:54:57 -0500

    add nix.conf to dotfiles

--- utils/setup-dotfiles.sh
+++ utils/setup-dotfiles.sh
@@ -5,11 +5,13 @@ rm ~/.bashrc
 rm ~/.config/fish/config.fish
 rm ~/.gitconfig
 rm ~/.config/Code/User/settings.json
+rm ~/.config/nix/nix.conf
 
 # Create symlink and force overwrite
 ln -sf ~/home/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/home/dotfiles/config.fish ~/.config/fish/config.fish
 ln -sf ~/home/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/home/dotfiles/vscode-settings.json ~/.config/Code/User/settings.json
+ln -sf ~/home/dotfiles/nix.conf ~/.config/nix/nix.conf
 
 echo "Dotfiles updated."
