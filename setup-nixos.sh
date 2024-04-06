79175a920a95bf7252d477c9356e9c5be9d91ea9
Jason Lieb <Jason.lieb@outlook.com>
2024-04-06 01:11:57 -0400

    update

--- setup-nixos.sh
+++ setup-nixos.sh
@@ -8,7 +8,7 @@ mkdir -p ~/.ssh
 printf "\nSetting up ssh key..."
 printf "\nWhat is your email address? "
 read email_address
-sudo yes '' | ssh-keygen -t ed25519 -C $email_address &> /dev/null
+yes '' | ssh-keygen -t ed25519 -C $email_address &> /dev/null
 eval "$(ssh-agent -s)" &> /dev/null
 ssh-add $HOME/.ssh/id_ed25519
 
