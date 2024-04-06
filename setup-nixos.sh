9ed862670dc2599623b135508ad920606d8cff0a
Jason Lieb <Jason.lieb@outlook.com>
2024-04-06 00:30:36 -0400

    update

--- setup-nixos.sh
+++ setup-nixos.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+cd $HOME
 mkdir -p ~/.ssh
 
 printf "\nSetting up ssh key..."
@@ -9,7 +10,7 @@ printf "\nWhat is your email address? "
 read email_address
 yes '' | ssh-keygen -t ed25519 -C $email_address &> /dev/null
 eval "$(ssh-agent -s)" &> /dev/null
-ssh-add ~/.ssh/id_ed25519
+ssh-add $HOME/.ssh/id_ed25519
 
 printf "\nSetting up ssh key with Github...\n"
 read -p "What is the name of the ssh key? " ssh_key_name
@@ -25,4 +26,4 @@ sudo hostname $hostname
 sudo cp /etc/nixos/hardware-configuration.nix $HOME/home-nix/$hostname/hardware-configuration.nix
 
 echo "Setting up nix configuration..."
-sudo nixos-rebuild switch --flake ~/home-nix#$hostname
+sudo nixos-rebuild switch --flake $HOME/home-nix#$hostname
