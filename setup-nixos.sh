9b592c0fb6be3665e14a9f41d254202c936d92af
Jason Lieb <Jason.lieb@outlook.com>
2024-04-05 23:27:15 -0400

    update

--- setup-nixos.sh
+++ setup-nixos.sh
@@ -13,7 +13,7 @@ printf "\nSetting up ssh key..."
 printf "\nWhat is your email address? "
 read email_address
 yes '' | ssh-keygen -t ed25519 -C $email_address > /dev/null
-eval "$(ssh-agent -s)" > /dev/null
+eval "$(ssh-agent -s)" &> /dev/null
 ssh-add ~/.ssh/id_ed25519
 
 printf "\nSetting up ssh key with Github...\n"
@@ -22,12 +22,12 @@ read -p "What is the name of the ssh key? " ssh_key_name
 printf "\nCreating github cli shell and adding SSH key to GitHub..."
 nix-shell -p gh --run "gh auth login; gh ssh-key add ~/.ssh/id_ed25519.pub -t '$ssh_key_name'"
 
-printf "\nCloning nix configuration..."
+printf "\n"
 nix-shell -p git --run "git clone git@github.com:jason-lieb/home-nix.git"
 
 printf "\nWhat is the hostname of this computer? "
 read hostname
-sudo hostnamectl set-hostname $hostname
+sudo hostname $hostname
 sudo cp /etc/nixos/hardware-configuration.nix ~/home-nix/$hostname/hardware-configuration.nix
 
 echo "Setting up nix configuration..."
