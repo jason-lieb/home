a73b35580d32f95b63a29bb8fd501cd67200cb58
Jason Lieb <Jason.lieb@outlook.com>
2024-04-05 23:05:24 -0400

    update

--- setup-nixos.sh
+++ setup-nixos.sh
@@ -21,7 +21,7 @@ printf "\nSetting up ssh key with Github...\n"
 read -p "What is the name of the ssh key? " ssh_key_name
 
 printf "\nCreating github cli shell and adding SSH key to GitHub..."
-nix-shell -p gh --run "gh auth login; gh ssh-key add ~/.ssh/id_ed25519.pub -t '$ssh_key_name'"
+nix-shell -p gh --run "gh auth login; gh ssh-key add ~/.ssh/id_ed25519.pub -T '$ssh_key_name'"
 
 printf "\nCloning nix configuration..."
 nix-shell -p git --run "git clone git@github.com:jason-lieb/home-nix.git"
