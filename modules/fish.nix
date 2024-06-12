dc58721581f8e9d0baf6d404243805ab3a733d50
Jason Lieb <Jason.lieb@outlook.com>
2024-06-12 00:41:11 -0400

    update fish aliases

--- modules/fish.nix
+++ modules/fish.nix
@@ -9,6 +9,16 @@
     interactiveShellInit = ''
       set fish_greeting
       source (zoxide init fish | psub)
+      function st
+          switch (count $argv)
+              case 0
+                  stack test --fast --file-watch --watch-all fancy-api jobs
+              case 1
+                  stack test --fast --file-watch --watch-all --ta '-m "'$argv[1]'"' fancy-api jobs
+              case '*'
+                  echo "Too many arguments"
+          end
+      end
     '';
 
     shellInit = ''
@@ -16,43 +26,64 @@
     '';
 
     shellAliases = {
+      # General
       c = "clear";
-      cd = "z";
-      g = "git";
-      gac = "git add -A; git commit -m";
-      ls = "ls";
       la = "ls -A";
       ll = "ls -l";
       lr = "ls -R"; # recursive ls
-      m = "make";
-      mon-desk = "~/home/utils/switch-to-desk-monitors.sh";
-      mon-tv = "~/home/utils/switch-to-tv-monitor.sh";
-      main = "git checkout main";
-      pull = "git pull origin";
-      push = "git push origin";
-      fpush = "git push origin --force";
-      "run-qa" = "git commit --allow-empty -m '[qa]'";
-      "run-cy" = "git commit --allow-empty -m '[cy]'";
-      up = "make update";
-      bran = "git branch | tr '\\n' '\\n'";
-      dbran = "git branch -D";
-      nbran = "git checkout -b";
-      sbran = "git checkout";
-      "cd.." = "cd ..";
+      # Navigation
+      cd = "z";
       ".." = "cd ..";
       "..." = "cd ../..";
       "...." = "cd ../../..";
       "....." = "cd ../../../..";
-      suod = "sudo";
+      "......" = "cd ../../../../..";
+      # Git Basics
+      g = "git";
+      ga = "git add";
+      gap = "git add -p";
+      gc = "git commit -m";
+      gcs = "git commit --squash=HEAD -m 'squash'";
+      gac = "git add -A; git commit -m";
+      gacs = "git add -A; git commit --squash=HEAD -m 'squash'";
+      # Look into using git commit --fixup
+      gcp = "git cherry-pick";
+      main = "git checkout main";
+      pull = "git pull origin";
+      push = "git push origin";
+      fpush = "git push origin --force";
+      # Git Stash
+      gs = "git stash";
+      gsd = "git stash drop";
+      gsl = "git stash list";
+      gsp = "git stash pop";
+      # Git Branch
+      b = "git branch | tr '\\n' '\\n'";
+      db = "git branch -D";
+      nb = "git checkout -b";
+      sb = "git checkout";
+      re = "git fetch origin main && git rebase origin/main";
+      sq = "git rebase -i origin/main";
+      # Trigger Github Actions
+      "run-qa" = "git commit --allow-empty -m '[qa]'";
+      "run-cy" = "git commit --allow-empty -m '[cy]'";
+      "run-eph" = "git commit --allow-empty -m '[ephemeral]'";
+      # Misc
+      ghce = "gh copilot explain";
+      ghcs = "gh copilot suggest";
+      ghcu = "gh extension install github/gh-copilot --force";
+      mon-desk = "~/home/utils/switch-to-desk-monitors.sh";
+      mon-tv = "~/home/utils/switch-to-tv-monitor.sh";
       "enter-db" = ''
         docker exec -it freckle-megarepo-postgres bash -c "psql -U postgres -d classroom_dev"'';
-      "format-backend-whole" = "stack exec -- fourmolu -i .";
-      "format-backend" =
-        ''git diff --name-only HEAD "*.hs" | xargs fourmolu -i'';
-      rebase = "git fetch origin main && git rebase origin/main";
-      squash = "git rebase -i origin/main";
-      nb = "npm run build";
-      gcp = "git cherry-pick";
+      clear-docker-cache = "docker system prune -a";
+      mw =
+        "stack test --no-run-tests --fast --file-watch --watch-all fancy-api";
+      stf = "stack test --fast --file-watch --watch-all fancy-api";
+      stj = "stack test --fast --file-watch --watch-all jobs";
+      up = "make update";
+      down = "pushd ~/megarepo/backend; and make services.stop; and popd";
+      # Nix
       rs = "sudo nixos-rebuild switch --impure --flake /home/jason/home-nix#${
           builtins.getEnv "HOSTNAME"
         }";
@@ -70,5 +101,9 @@
       nix-clean =
         "sudo nix-collect-garbage --delete-older-than 3d && sudo /run/current-system/bin/switch-to-configuration boot";
     };
+    # Not Currently Needed
+    # "format-backend-whole" = "stack exec -- fourmolu -i .";
+    # "format-backend" =
+    #   ''git diff --name-only HEAD "*.hs" | xargs fourmolu -i'';
   };
 }
