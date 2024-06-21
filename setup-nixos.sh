04e58049730975875c29dcefd098342b1a94793a
Jason Lieb <Jason.lieb@outlook.com>
2024-06-20 22:05:18 -0400

    update setup script

--- setup-nixos.sh
+++ setup-nixos.sh
@@ -20,8 +20,8 @@ nix-shell -p git --run "git clone git@github.com:jason-lieb/home-nix.git"
 printf "\nWhat is the hostname of this computer? "
 read hostname
 sudo hostname $hostname
-mkdir -p $HOME/home-nix/modules/$hostname
-sudo cp /etc/nixos/hardware-configuration.nix $HOME/home-nix/modules/$hostname/hardware-configuration.nix
+mkdir -p $HOME/home-nix/$hostname
+sudo cp /etc/nixos/hardware-configuration.nix $HOME/home-nix/$hostname/hardware-configuration.nix
 
 printf "Setting up nix configuration...\n"
 nix-shell -p git --run "sudo nixos-rebuild switch --flake $HOME/home-nix#$hostname --impure"
