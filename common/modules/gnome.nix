8feb0d78de81ca039d44173f653241dbb15b04ff
Jason Lieb <Jason.lieb@outlook.com>
2024-06-23 20:40:35 -0400

    create separate laptop gnome module

--- common/modules/gnome.nix
+++ common/modules/gnome.nix
@@ -46,100 +46,106 @@
 
     profiles.user.databases = [
       {
-        settings = with lib.gvariant; {
-          "org/gnome/desktop/background" = {
-            color-shading-type = "solid";
-            picture-options = "zoom";
-            picture-uri = "file://" + ../wallpaper.png;
-          };
+        settings =
+          with lib.gvariant;
+          let
+            commonSettings = {
+              "org/gnome/desktop/background" = {
+                color-shading-type = "solid";
+                picture-options = "zoom";
+                picture-uri = "file://" + ../wallpaper.png;
+              };
 
-          "org/gnome/mutter".experimental-features =
-            if builtins.getEnv "HOSTNAME" == "laptop" then [ "scale-monitor-framebuffer" ] else [ ];
+              "org/gnome/desktop/wm/keybindings".switch-windows = [ "<Alt>Tab" ];
+              "org/gnome/desktop/wm/keybindings".switch-windows-backwards = [ "<Shift><Alt>Tab" ];
 
-          "org/gnome/desktop/wm/keybindings".switch-windows = [ "<Alt>Tab" ];
-          "org/gnome/desktop/wm/keybindings".switch-windows-backwards = [ "<Shift><Alt>Tab" ];
+              "org/gnome/desktop/interface" = {
+                clock-format = "12h";
+                color-scheme = "prefer-dark";
+              };
 
-          "org/gnome/desktop/interface" = {
-            clock-format = "12h";
-            color-scheme = "prefer-dark";
-          };
+              "org/gnome/desktop/notifications/application/gnome-power-panel" = {
+                application-id = "gnome-power-panel.desktop";
+                enable = false;
+              };
 
-          "org/gnome/desktop/notifications/application/gnome-power-panel" = {
-            application-id = "gnome-power-panel.desktop";
-            enable = false;
-          };
+              "org/gnome/desktop/peripherals/touchpad" = {
+                speed = 0.5;
+                tap-to-click = true;
+              };
 
-          "org/gnome/desktop/peripherals/touchpad".tap-to-click = true;
+              "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
 
-          "org/gnome/desktop/peripherals/keyboard".numlock-state = true;
+              "org/gnome/desktop/screensaver".lock-enabled = false;
 
-          "org/gnome/desktop/screensaver".lock-enabled = false;
+              "org/gnome/desktop/wm/preferences" = {
+                auto-raise = false;
+                button-layout = "appmenu:minimize,maximize,close";
+                focus-mode = "click";
+                num-workspace = 4.0; # Doesn't work
+              };
 
-          "org/gnome/desktop/wm/preferences" = {
-            auto-raise = false;
-            button-layout = "appmenu:minimize,maximize,close";
-            focus-mode = "click";
-            num-workspace = 4.0; # Doesn't work
-          };
+              "org/gnome/mutter" = {
+                edge-tiling = true;
+                center-new-windows = true;
+                dynamic-workspaces = false;
+                workspaces-only-on-primary = true;
+              };
 
-          "org/gnome/mutter" = {
-            edge-tiling = true;
-            center-new-windows = true;
-            dynamic-workspaces = false;
-            workspaces-only-on-primary = true;
-          };
+              "org/gnome/settings-daemon/plugins/color" = {
+                night-light-enabled = true;
+                night-light-temperature = 3700.0;
+              };
 
-          "org/gnome/settings-daemon/plugins/color" = {
-            night-light-enabled = true;
-            night-light-temperature = 3700.0;
-          };
+              "org/gnome/settings-daemon/plugins/power" = {
+                sleep-inactive-ac-timeout = 3600.0;
+                sleep-inactive-ac-type = "suspend";
+                power-mode = "performance";
+              };
 
-          "org/gnome/settings-daemon/plugins/power" = {
-            sleep-inactive-ac-timeout = 3600.0;
-            sleep-inactive-ac-type = "suspend";
-            power-mode = "performance";
-          };
+              "org/gnome/shell" = {
+                disable-user-extensions = false;
+                enabled-extensions = [
+                  "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
+                  "caffeine@patapon.info"
+                  "display-brightness-ddcutil@themightydeity.github.com"
+                ];
+                favorite-apps = [
+                  "org.gnome.Nautilus.desktop"
+                  "brave-browser.desktop"
+                  "obsidian.desktop"
+                  "Alacritty.desktop"
+                  "code.desktop"
+                  "org.gnome.Software.desktop"
+                ];
+              };
 
-          "org/gnome/shell" = {
-            disable-user-extensions = false;
-            enabled-extensions = [
-              "launch-new-instance@gnome-shell-extensions.gcampax.github.com"
-              "caffeine@patapon.info"
-              "display-brightness-ddcutil@themightydeity.github.com"
-            ];
-            favorite-apps = [
-              "org.gnome.Nautilus.desktop"
-              "brave-browser.desktop"
-              "obsidian.desktop"
-              "Alacritty.desktop"
-              "code.desktop"
-              "org.gnome.Software.desktop"
-            ];
-          };
+              "org/gnome/shell/app-switcher".current-workspace-only = true;
 
-          "org/gnome/shell/app-switcher".current-workspace-only = true;
+              "org/gnome/shell/extensions/caffeine" = {
+                duration-timer = 2.0;
+                indicator-position-max = 4.0;
+                screen-blank = "always";
+                show-notifications = false;
+              };
 
-          "org/gnome/shell/extensions/caffeine" = {
-            duration-timer = 2.0;
-            indicator-position-max = 4.0;
-            screen-blank = "always";
-            show-notifications = false;
-          };
+              "org/gnome/shell/extensions/display-brightness-ddcutil" = {
+                ddcutil-binary-path = "/run/current-system/sw/bin/ddcutil";
+                ddcutil-queue-ms = 130.0;
+                ddcutil-sleep-multiplier = 40.0;
+                decrease-brightness-shortcut = [ "<Control>XF86MonBrightnessDown" ];
+                increase-brightness-shortcut = [ "<Control>XF86MonBrightnessUp" ];
+                only-all-slider = true;
+                position-system-menu = 3.0;
+                show-all-slider = true;
+                step-change-keyboard = 2.0;
+              };
 
-          "org/gnome/shell/extensions/display-brightness-ddcutil" = {
-            ddcutil-binary-path = "/run/current-system/sw/bin/ddcutil";
-            ddcutil-queue-ms = 130.0;
-            ddcutil-sleep-multiplier = 40.0;
-            decrease-brightness-shortcut = [ "<Control>XF86MonBrightnessDown" ];
-            increase-brightness-shortcut = [ "<Control>XF86MonBrightnessUp" ];
-            only-all-slider = true;
-            position-system-menu = 3.0;
-            show-all-slider = true;
-            step-change-keyboard = 2.0;
-          };
-
-          "org/gnome/tweaks".show-extensions-notice = false;
-        };
+              "org/gnome/tweaks".show-extensions-notice = false;
+            };
+            laptopSettings = import ../../laptop/gnome.nix;
+          in
+          if builtins.getEnv "HOSTNAME" == "laptop" then commonSettings // laptopSettings else commonSettings;
       }
     ];
   };
