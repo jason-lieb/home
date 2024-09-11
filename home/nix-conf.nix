ade9494bd1f3ad50cd3001dff1e273c1077189aa
Jason Lieb <Jason.lieb@outlook.com>
2024-09-10 21:53:15 -0400

    fix nix config

--- home/nix-conf.nix
+++ home/nix-conf.nix
@@ -1,14 +1,8 @@
 { home-manager }:
 {
-  home.file =
-    if home-manager then
-      {
-        ".config/nix/nix.conf".text = ''
-          experimental-features = nix-command flakes
-          auto-optimise-store = true
-          netrc-file = /home/jason/.config/nix/netrc
-        '';
-      }
-    else
-      { };
+  home.file.".config/nix/nix.conf".text = ''
+    experimental-features = nix-command flakes
+    auto-optimise-store = true
+    netrc-file = /home/jason/.config/nix/netrc
+  '';
 }
