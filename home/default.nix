0dd5a9c9df9c882df7331a0ebaf00f6bf64f7d05
Jason Lieb <Jason.lieb@outlook.com>
2024-09-17 13:57:23 -0400

    add direnv reminder

--- home/default.nix
+++ home/default.nix
@@ -24,6 +24,11 @@
     (import ./vscode.nix { inherit pkgs vscode-extensions; })
   ];
 
+  # programs.direnv = {
+  #   enable = true;
+  #   nix-direnv.enable = true;
+  # };
+
   home.packages =
     (with pkgs; [
       alacritty
