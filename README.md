a05e749607b25099cd713c16dd489a5d1972a65c
Jason Lieb <Jason.lieb@outlook.com>
2024-12-17 01:19:50 -0500

    re add profiles and shell in main repo

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ```
 bash -c "$(curl -fsSl https://raw.githubusercontent.com/jason-lieb/home/main/setup-nixos.sh)"
 ```
+
+## Nix Profiles Use
+
+```
+nix profile install ./profiles#backend
+```
---
+++ profiles/flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "Nixos Profiles for Global Tools";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    freckle.url = "github:freckle/flakes?dir=main";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      nixpkgs,
+      freckle,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        fourmolu = freckle.packages.${system}.fourmolu-0-13-x;
+        haskell =
+          version:
+          (freckle.lib.${system}.haskellBundle {
+            ghcVersion = version;
+            enableHLS = true;
+          });
+      in
+      {
+        packages = rec {
+          default = backend;
+          backend = pkgs.buildEnv {
+            name = "backend";
+            paths = [
+              fourmolu
+              (haskell "ghc-9-2-8")
+            ];
+          };
+          curricula = pkgs.buildEnv {
+            name = "curricula";
+            paths = [
+              fourmolu
+              (haskell "ghc-9-6-6")
+            ];
+          };
+        };
+      }
+    );
+
+  nixConfig = {
+    extra-substituters = [
+      "https://freckle.cachix.org"
+      "https://freckle-private.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "freckle.cachix.org-1:WnI1pZdwLf2vnP9Fx7OGbVSREqqi4HM2OhNjYmZ7odo="
+      "freckle-private.cachix.org-1:zbTfpeeq5YBCPOjheu0gLyVPVeM6K2dc1e8ei8fE0AI="
+    ];
+  };
+}
