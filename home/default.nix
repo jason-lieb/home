e57c44c3f71236c6ef3a18220680b0036c417db5
Jason Lieb <Jason.lieb@outlook.com>
2025-01-09 19:16:27 -0500

    move git to default home file

--- home/default.nix
+++ home/default.nix
@@ -20,7 +20,6 @@ in
 
   imports = [
     ./shells.nix
-    ./git.nix
     ./vscode.nix
   ];
 
@@ -56,6 +55,28 @@ in
       "video/mp4" = "brave-browser.desktop";
       "image/jpeg" = "org.gnome.Loupe.desktop"; # Loupe = Image Viewer
       "image/png" = "org.gnome.Loupe.desktop";
+    };
+  };
+
+  programs.git = {
+    enable = true;
+
+    userName = "Jason Lieb";
+    userEmail = "Jason.lieb@outlook.com";
+
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.editor = "code --wait";
+      fetch.prune = true;
+      merge.ff = "only";
+      #merge.tool = "nvimdiff";
+      pull.ff = "only";
+      pull.autostash = true;
+      push.default = "current";
+      rebase.autoSquash = true;
+      rebase.autoStash = true;
+      rebase.stat = true;
+      rerere.enabled = true;
     };
   };
 
