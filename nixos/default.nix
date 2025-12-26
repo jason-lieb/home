3a48091b8f2b015c54340c1333731d0b44e62a94
Jason Lieb <Jason.lieb@outlook.com>
2025-12-26 15:30:42 -0500

    install steam

--- nixos/default.nix
+++ nixos/default.nix
@@ -158,6 +158,13 @@
 
   programs.appimage.enable = true;
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
+
   services.syncthing = {
     enable = true;
     user = "jason";
