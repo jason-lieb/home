93cbe21181ad307188d39c90dda4ebd8082d68bf
Jason Lieb <Jason.lieb@outlook.com>
2026-02-19 03:50:56 -0500

    add configuration limit on mini to save boot space

--- nixos/mini/default.nix
+++ nixos/mini/default.nix
@@ -5,6 +5,7 @@
   imports = [ ./hardware.nix ];
 
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
 
   swapDevices = [{
